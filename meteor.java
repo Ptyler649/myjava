4ef3bfba3d5c268d269597e485eecf76b193444e
Philip Tyler <Ptyler649@gmail.com>
2013-05-08 23:30:10 +0100

    added go class
    added go class to test this furter

--- meteor.java
+++ meteor.java
@@ -2,6 +2,14 @@ public class meteor{
 	public static void main(String[] args)
 	{
 		System.out.println("Meteor - fireball");
+		
+		meteor mymeteor = new meteor();
+		mymeteor.go();
 	}
 
+	void go()
+	{
+		System.out.println("big fireball");
+	}
+	
 }
