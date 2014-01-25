1c53f745fe80341bc080f0e3e7492ce29ed09b78
Philip Tyler <Ptyler649@gmail.com>
2014-01-25 19:42:28 +0000

    added time date 24hr to standard code

--- myjarvis.java
+++ myjarvis.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.*;
 
 public class myjarvis{
 	public static void main(String[] args)
@@ -9,10 +10,14 @@ public class myjarvis{
 		BufferedReader myread = new BufferedReader (new InputStreamReader(System.in));
 
 
+		
+		
+
 		int x = 0;
 
 		while (x != 99)
 		{
+			// take input
 			try
 			{
 				myinput = myread.readLine();
@@ -23,6 +28,7 @@ public class myjarvis{
 			}
 
 
+			// greetings
 			if(myinput.equals("fine"))
 			{
 				System.out.println("I am glad to here that sir!!!");
@@ -32,6 +38,37 @@ public class myjarvis{
 				System.out.println("ok, sir!");
 			}
 
+			// appointment for today
+			// get sysdate
+			Date d = new Date();
+			String sysdate = d.toString();
+			System.out.println(sysdate);
+
+			String currTime24 = sysdate.substring(11,16);
+
+			
+			// convert to standard day time
+			String currTimeSTD = sysdate.substring(11,13);
+			int foo = Integer.parseInt(currTimeSTD);
+			if(foo > 12)
+			{
+				foo = foo - 12;
+			};
+			currTimeSTD = foo + ":" + sysdate.substring(14,16);
+			// need to pad with 0's
+
+
+
+			System.out.println("24 clock time is :"+currTime24);
+			System.out.println("Standard clock time is : "+currTimeSTD);
+
+
+
+
+		
+
+
+			// bye
 			if(myinput.equals("bye"))
 			{
 				x = 99;
