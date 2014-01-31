d1411a7dbc2f18d2cbed0a10a76d017963110935
Philip Tyler <Ptyler649@gmail.com>
2014-01-31 11:44:45 +0000

    fixed various
    list creation and removal working now will working on individual items
    from list removal

--- myjarvis.java
+++ myjarvis.java
@@ -5,17 +5,45 @@ import java.util.*;
 public class myjarvis{
 	public static void main(String[] args)
 	{
+
+		// vars
 		String myinput = "";
-		System.out.println("Good day sir, how are you?");
-
 		BufferedReader myread = new BufferedReader (new InputStreamReader(System.in));
+		File myfile = new File("jarvismem.txt");
 
+
+		// get sysdate
+		Date d = new Date();
+		String sysdate = d.toString();
+		String currTime24 = sysdate.substring(11,16);
+		String currTimeSTD = sysdate.substring(11,13);
+		int foo = Integer.parseInt(currTimeSTD);
+		if(foo > 12)
+		{
+			foo = foo - 12;
+		};
+		currTimeSTD = foo + ":" + sysdate.substring(14,16);
+		if(foo < 10)
+		{
+			currTimeSTD = "0"+currTimeSTD;
+		};
+		//System.out.println("24 clock time is :"+currTime24);
+		//System.out.println("Standard clock time is : "+currTimeSTD);
 
 		
-		
+		// Greeting
+		if(Integer.parseInt(sysdate.substring(11,13)) < 13)
+		{
+			System.out.println("Good morning sir, how are you?");		
+		}
+		else
+		{
+			System.out.println("Good afternoon sir, how are you?");		
+		}
 
+
+		// infinite loop
 		int x = 0;
-
 		while (x != 99)
 		{
 			// take input
@@ -29,7 +57,7 @@ public class myjarvis{
 			}
 
 
-			// greetings
+			// greeting response
 			if(myinput.equals("fine"))
 			{
 				System.out.println("I am glad to here that sir!!!");
@@ -39,61 +67,138 @@ public class myjarvis{
 				System.out.println("ok, sir!");
 			}
 
-			// appointment for today
-			// get sysdate
-			Date d = new Date();
-			String sysdate = d.toString();
-			System.out.println(sysdate);
 
-			String currTime24 = sysdate.substring(11,16);
 
-			
-			// convert to standard day time
-			String currTimeSTD = sysdate.substring(11,13);
-			int foo = Integer.parseInt(currTimeSTD);
-			if(foo > 12)
+			// check remember
+			boolean retval = myinput.contains("remember");			
+			if(retval)
 			{
-				foo = foo - 12;
-			};
-			currTimeSTD = foo + ":" + sysdate.substring(14,16);
-			// pad 0's
-			if(foo < 10)
-			{
-				currTimeSTD = "0"+currTimeSTD;
-			};
-
-			System.out.println("24 clock time is :"+currTime24);
-			System.out.println("Standard clock time is : "+currTimeSTD);
-
 
 			// split REMEMBER DESC TIME
 			String checkRemember ="";
-			System.out.println(myinput.length());
+			//System.out.println(myinput.length());
 			if(myinput.length() > 7)
 			{		
 				checkRemember = myinput.substring(0,8);
 			}
 
-			File myfile = new File("jarvismem.txt");
+			//File myfile = new File("jarvismem.txt");
 			if(checkRemember.equals("remember"))
 			{
-			// store a reminder
-
-			try
-			{
-				System.out.println("create a new file");
-				//File myfile = new File("jarvismem.txt");
-				myfile.createNewFile();
-				PrintWriter pw = new PrintWriter(myfile);
-				pw.println("testing testing");
-				pw.close();
-			}
-			catch(Exception e)
-			{
-				e.printStackTrace();
-			}
+				// store a reminder
+				String actualNote = myinput.substring(9,myinput.length());
+				try
+				{
+					myfile.createNewFile();
+					BufferedWriter output = new BufferedWriter(new FileWriter(myfile, true));
+					output.write(actualNote);
+					output.newLine();
+					output.close();
+				}
+				catch(Exception e)
+				{
+					e.printStackTrace();
+				}
+				}
 			}
 				
+			// List - list what is is the mem list
+			retval = myinput.contains("what have we got jarvis");
+			if(retval)
+			{
+				if(myfile.exists())
+				{			
+				
+					try
+					{
+						BufferedReader in = new BufferedReader(new FileReader(myfile));
+						String line;
+						while((line = in.readLine()) != null)
+						{
+    						System.out.println(line);
+						}
+						in.close();
+					}
+					catch(Exception e)
+					{
+						e.printStackTrace();
+					}
+				}
+			
+				else
+				{
+					System.out.println("no list yet, sir - would you like to remember something for later?");
+				}
+			}	
+
+
+
+			// start a new list by deleting the old list
+			retval = myinput.contains("new list");	
+			//System.out.println(retval);
+
+			if(retval)
+			{
+				try
+				{
+					if(myfile.exists())
+					{
+						//System.out.println("Delete the file");
+						System.gc();
+						myfile.delete();
+					}
+				}
+				catch(Exception e)
+				{
+					e.printStackTrace();
+				}
+			}			
+
+
+			// removal of a particular item from the list
+			// jarvis remove <variable> from the list
+			retval = myinput.contains("jarvis remove");
+			if(retval)
+			{
+				//System.out.println("Remove: "+myinput.length());
+				//System.out.println(myinput.substring(14,myinput.length()));
+
+				if(myfile.exists())
+				{			
+				
+					try
+					{
+						String checkedInput = myinput.substring(14,myinput.length());
+						BufferedReader an = new BufferedReader(new FileReader(myfile));
+						String line;
+						while((line = an.readLine()) != null)
+						{
+    						//System.out.println(line);
+    						if(checkedInput.equals(line))
+    						{
+    							System.out.println("Found:"+checkedInput);
+    						}
+    						else
+    						{
+    							System.out.println("Not Found:"+line);	
+    						}
+    						// add to an array only if value does not match
+						}
+						// output array creating the new file - the matched value above						
+						// then print out contents of the file
+						an.close();
+					}
+					catch(Exception e)
+					{
+						e.printStackTrace();
+					}
+				}
+			}
+
+
+
+
+
 
 			// check if a reminder is required at this time
 			if(myfile.exists())
@@ -105,7 +210,7 @@ public class myjarvis{
 
 				// check reminders in file
 				String aline = br.readLine();
-				System.out.println("LINE: "+aline);
+				//System.out.println("LINE: "+aline);
 			}
 			catch(Exception f)
 			{
