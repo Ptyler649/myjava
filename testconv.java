9dbf46ec47aa9f79c01452c77fc6870b4be4d0d8
ptyler649 <ptyler649@gmail.com>
2017-11-14 10:04:24 +0000

    ammendments ot watchdir for generic testing env

---
+++ testconv.java
@@ -0,0 +1,8 @@
+public class testconv{
+	public static void main(String[] args){
+		String x = "39874.00";
+		double out = Double.parseDouble(x);
+		//int out = Integer.parseInt(x);
+		System.out.println("out:"+out);
+	}
+}
