05dec8c09dc3889aca1e2513105dca6e7dd47037
ptyler649 <ptyler649@gmail.com>
2021-12-05 11:24:04 +0000

    testing

---
+++ somestuff/poly.java
@@ -0,0 +1,22 @@
+
+
+public class poly{
+
+    public static void main(String[] args){
+
+        Animal myAnimal = new Animal();
+
+        myAnimal.eat();
+
+    }
+
+};
+
+class Animal{
+    
+    void eat(){
+            System.out.println("chomp chomp");
+    }
+}
+
+
