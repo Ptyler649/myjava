7e024aa996056924ee3455d8120cc1f4ad5982e3
ptyler649 <ptyler649@gmail.com>
2021-12-06 09:29:32 +0000

    packages working

---
+++ somestuff/mypackages/atest.java
@@ -0,0 +1,9 @@
+package mypackages;
+
+public class atest{
+
+        public void test1(){
+            System.out.println("test 1");
+        }
+
+}
---
+++ somestuff/testsomething.java
@@ -0,0 +1,13 @@
+import mypackages.*;
+
+public class testsomething{
+
+    public static void main(String[] args){
+
+            atest a = new atest();
+            a.test1();
+
+    }
+
+
+}
